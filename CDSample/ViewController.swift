51d3c47e431b3126a29de32b81931d43f079f049
Justin Davis <jjustind@Justins-MacBook-Pro.local>
2015-12-11 00:03:53 -0600

    Initial Commit - Simple array to save data temporarily

--- CDSample/ViewController.swift
+++ CDSample/ViewController.swift
@@ -7,18 +7,69 @@
 //
 
 import UIKit
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var names: [String] = []
+    
+    @IBAction func addButton(sender: AnyObject) {
+       let alert = UIAlertController(title: "Add String", message: "Add new String", preferredStyle: .Alert)
+        let save = UIAlertAction(title: "Save", style: .Default) { (action: UIAlertAction) -> Void in
+            let textField = alert.textFields![0] as UITextField
+            self.names.append(textField.text!)
+            self.tableView.reloadData()
+        }
+        
+        let cancel = UIAlertAction(title: "Cancel", style: .Default) { (action: UIAlertAction) -> Void in
+        }
+        
+        alert.addTextFieldWithConfigurationHandler { (textField: UITextField) -> Void in
+        }
+        
+        alert.addAction(save)
+        alert.addAction(cancel)
+        
+        presentViewController(alert, animated: true, completion: nil)
+        
+    }
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tableView.registerClass(UITableViewCell.self, forCellReuseIdentifier:"Cell")
+        title = "Basic String Storage"
+        
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    // Table view delegate methods
+    
+    func numberOfSectionsInTableView(tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("Cell", forIndexPath: indexPath);
+        cell.textLabel?.text = names[indexPath.row]
+        return cell
+        
+    }
+    
+    
 
 
 }
